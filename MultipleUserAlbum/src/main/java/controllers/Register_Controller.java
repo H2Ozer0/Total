f8798f043b40be532fb498117f163dc1e0a9879f
smj <smj23>
2023-12-06 09:14:48 +0800

    完善了注册界面，并且注册界面可以提供新建用户所需要的参数

--- MultipleUserAlbum/src/main/java/controllers/Register_Controller.java
+++ MultipleUserAlbum/src/main/java/controllers/Register_Controller.java
@@ -22,10 +22,6 @@ public class Register_Controller {
 
 
     @RequestMapping("/register_page")
-    public String registerPage(HttpServletRequest request, Model model) {
-        return "register";
-    }
-    @RequestMapping("/register_page")
     public String resgister_page(HttpServletRequest request, Model model)
     {
         return "register";
@@ -39,15 +35,20 @@ public class Register_Controller {
      */
     @RequestMapping("/Regestion")
     @ResponseBody
-    public String register(@RequestParam String username, @RequestParam String password, @RequestParam String email, boolean isAdmin, String description) {
+    public String register(@RequestParam String username, @RequestParam String password, @RequestParam String email, boolean isAdmin, @RequestParam String description) {
         // 检查用户名是否已被占用
         if (userService.existsByUsername(username)) {
             return "have existed！";
         }
 
-        String userId = String.valueOf(userService.getTotalNumberOfUser() + 1); 
+        String userId = String.valueOf(userService.getTotalNumberOfUser() + 1);
+
+        /**
+         * 在这里原来是 userService.saveUser(userID, username, password, email, isAdmin, description);
+         * 可是由于userService.getTotalNumberOfUser() 有错误，导致主键重复，所以改用用户名作为用户ID
+         */
         // 将用户保存到数据库（你需要在 UserService 中实现这个方法
-        userService.saveUser(userId, username, password, email, isAdmin, description);
+        userService.saveUser(username, username, password, email, isAdmin, description);
         // 返回响应内容
         return "Received username: " + username + ", password: " + password;
     }
