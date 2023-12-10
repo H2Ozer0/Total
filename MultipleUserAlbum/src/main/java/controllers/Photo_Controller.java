c9e7ab8217380e49d82b7a6dcc7b8232b63060a3
aWazINg7 <10210103@njnu.edu.cn>
2023-12-10 22:26:19 +0800

    修改Photo_Controller类中showUploadForm

--- MultipleUserAlbum/src/main/java/controllers/Photo_Controller.java
+++ MultipleUserAlbum/src/main/java/controllers/Photo_Controller.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.sql.Timestamp;
+
 
 import java.util.List;
 
@@ -26,16 +29,11 @@ public class Photo_Controller {
 
     @GetMapping("/upload")
     public String showUploadForm(Model model) {
-        model.addAttribute("photo", new Photo()); // 用于绑定上传表单的实体
+        model.addAttribute("photo", new Photo(0, "", "", null, false));
+// 用于绑定上传表单的实体
         return "upload-form";
     }
 
-    @PostMapping("/upload")
-    public String handleUpload(@ModelAttribute("photo") Photo photo) {
-        // 处理上传逻辑
-        photoDAO.insertPhoto(photo);
-        return "redirect:/photos/list";
-    }
 
     @GetMapping("/delete/{photoID}")
     public String deletePhoto(@PathVariable int photoID) {
@@ -43,6 +41,25 @@ public class Photo_Controller {
         photoDAO.deletePhoto(photoID);
         return "redirect:/photos/list";
     }
+@PostMapping("/upload")
+public String handleUpload(@ModelAttribute("photo") Photo photo,
+                           @RequestParam("file") MultipartFile file) {
+    // 处理上传逻辑
+    // 在这里你可以将上传的文件保存到磁盘或云存储，然后将文件路径设置到照片对象中
+    // 例如：photo.setPath("/path/to/uploaded/file.jpg");
+
+    // 设置上传时间等其他属性
+    photo.setUploadTime(new Timestamp(System.currentTimeMillis()));
+    photo.setDeleted(false);
+
+    // 保存到数据库
+    photoDAO.insertPhoto(photo);
+
+    return "redirect:/photos/list";
+}
+
+
+
 
     @GetMapping("/edit/{photoID}")
     public String showEditForm(@PathVariable int photoID, Model model) {
